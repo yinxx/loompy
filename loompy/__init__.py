a7716fdfc3b4d2455eb09a1f7818fddcf8cbe4bc
Sten Linnarsson <sten.linnarsson@ki.se>
2017-08-15 17:46:13 +0200

    ok give up on circular dependencies

--- loompy/__init__.py
+++ loompy/__init__.py
@@ -1,3 +1,1 @@
-from .loom_layer import MemoryLoomLayer, LoomLayer
-from .loom_view import LoomView
-from .loompy import connect, create, combine, create_from_cellranger, LoomConnection
+from .loompy import connect, create, combine, create_from_cellranger, LoomConnection, MemoryLoomLayer, LoomLayer, LoomView
--- loompy/loompy.py
+++ loompy/loompy.py
@@ -1196,3 +1196,148 @@ def connect(filename: str, mode: str = 'r+') -> LoomConnection:
 		A LoomConnection instance.
 	"""
 	return LoomConnection(filename, mode)
+
+
+class MemoryLoomLayer():
+	def __init__(self, name: str, matrix: np.ndarray) -> None:
+		self.name = name
+		self.shape = matrix.shape
+		self.values = matrix
+
+	def __getitem__(self, slice: Tuple[Union[int, slice], Union[int, slice]]) -> np.ndarray:
+		return self.values[slice]
+
+	def __setitem__(self, slice: Tuple[Union[int, slice], Union[int, slice]], data: np.ndarray) -> None:
+		self.values[slice] = data
+
+	def sparse(self, genes: np.ndarray, cells: np.ndarray) -> scipy.sparse.coo_matrix:
+		return scipy.sparse.coo_matrix(self.values[genes, :][:, cells])
+
+
+class LoomLayer():
+	def __init__(self, ds: loompy.LoomConnection, name: str, dtype: str) -> None:
+		self.ds = ds
+		self.name = name
+		self.dtype = dtype
+		self.shape = ds.shape
+
+	def __getitem__(self, slice: Tuple[Union[int, slice], Union[int, slice]]) -> np.ndarray:
+		if self.name == "":
+			return self.ds._file['/matrix'].__getitem__(slice)
+		return self.ds._file['/layers/' + self.name].__getitem__(slice)
+
+	def __setitem__(self, slice: Tuple[Union[int, slice], Union[int, slice]], data: np.ndarray) -> None:
+		if self.name == "":
+			self.ds._file['/matrix'].__setitem__(slice, data.astype(self.dtype))
+		else:
+			self.ds._file['/layers/' + self.name].__setitem__(slice, data.astype(self.dtype))
+
+	def sparse(self, genes: np.ndarray, cells: np.ndarray) -> scipy.sparse.coo_matrix:
+		n_genes = self.ds.shape[0] if genes is None else genes.shape[0]
+		n_cells = self.ds.shape[1] if cells is None else cells.shape[0]
+		data: np.ndarray = None
+		row: np.ndarray = None
+		col: np.ndarray = None
+		for (ix, selection, vals) in self.ds.batch_scan(genes=genes, cells=cells, axis=1, layer=self.name):
+			nonzeros = np.where(vals > 0)
+			if data is None:
+				data = vals[nonzeros]
+				row = nonzeros[0]
+				col = selection[nonzeros[1]]
+			else:
+				data = np.concatenate([data, vals[nonzeros]])
+				row = np.concatenate([row, nonzeros[0]])
+				col = np.concatenate([col, selection[nonzeros[1]]])
+		return scipy.sparse.coo_matrix((data, (row, col)), shape=(n_genes, n_cells))
+		
+	def resize(self, size: Tuple[int, int], axis: int = None) -> None:
+		"""Resize the dataset, or the specified axis.
+		The dataset must be stored in chunked format; it can be resized up to the "maximum shape" (keyword maxshape) specified at creation time.
+		The rank of the dataset cannot be changed.
+		"Size" should be a shape tuple, or if an axis is specified, an integer.
+		BEWARE: This functions differently than the NumPy resize() method!
+		The data is not "reshuffled" to fit in the new shape; each axis is grown or shrunk independently.
+		The coordinates of existing data are fixed.
+		"""
+		if self.name == "":
+			self.ds._file['/matrix'].resize(size, axis)
+		else:
+			self.ds._file['/layers/' + self.name].resize(size, axis)
+
+
+class LoomView:
+	"""
+	An in-memory loom dataset
+	"""
+	def __init__(self, layers: Dict[str, loompy.MemoryLoomLayer], row_attrs: Dict[str, np.ndarray], col_attrs: Dict[str, np.ndarray]) -> None:
+		self.layers = layers
+		self.shape = next(iter(self.layers.values())).shape
+		self.row_attrs = row_attrs
+		self.col_attrs = col_attrs
+
+	def __getitem__(self, slice: Tuple[Union[int, slice], Union[int, slice]]) -> np.ndarray:
+		"""
+		Get a slice of the main matrix.
+
+		Args:
+			slice:		A slice object (see http://docs.h5py.org/en/latest/high/dataset.html)
+
+		Returns:
+			A numpy matrix
+		"""
+		return self.layers[""][slice]
+	
+	def _repr_html_(self) -> str:
+		"""
+		Return an HTML representation of the loom view, showing the upper-left 10x10 corner.
+		"""
+		rm = min(10, self.shape[0])
+		cm = min(10, self.shape[1])
+		html = "<p>"
+		html += "(" + str(self.shape[0]) + " genes, " + str(self.shape[1]) + " cells, " + str(len(self.layers)) + " layers)<br/>"
+		html += "<table>"
+		# Emit column attributes
+		for ca in self.col_attrs.keys():
+			html += "<tr>"
+			for ra in self.row_attrs.keys():
+				html += "<td>&nbsp;</td>"  # Space for row attrs
+			html += "<td><strong>" + ca + "</strong></td>"  # Col attr name
+			for v in self.col_attrs[ca][:cm]:
+				html += "<td>" + str(v) + "</td>"
+			if self.shape[1] > cm:
+				html += "<td>...</td>"
+			html += "</tr>"
+
+		# Emit row attribute names
+		html += "<tr>"
+		for ra in self.row_attrs.keys():
+			html += "<td><strong>" + ra + "</strong></td>"  # Row attr name
+		html += "<td>&nbsp;</td>"  # Space for col attrs
+		for v in range(cm):
+			html += "<td>&nbsp;</td>"
+		if self.shape[1] > cm:
+			html += "<td>...</td>"
+		html += "</tr>"
+
+		# Emit row attr values and matrix values
+		for row in range(rm):
+			html += "<tr>"
+			for ra in self.row_attrs.keys():
+				html += "<td>" + str(self.row_attrs[ra][row]) + "</td>"
+			html += "<td>&nbsp;</td>"  # Space for col attrs
+
+			for v in self[row, :cm]:
+				html += "<td>" + str(v) + "</td>"
+			if self.shape[1] > cm:
+				html += "<td>...</td>"
+			html += "</tr>"
+		# Emit ellipses
+		if self.shape[0] > rm:
+			html += "<tr>"
+			for v in range(rm + 1 + len(self.row_attrs.keys())):
+				html += "<td>...</td>"
+			if self.shape[1] > cm:
+				html += "<td>...</td>"
+			html += "</tr>"
+		html += "</table>"
+		return html
