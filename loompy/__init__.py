09a0d359b619adb5ba45b045fa8db28f27059fab
Sten Linnarsson <sten.linnarsson@ki.se>
2017-08-15 17:41:10 +0200

    x

--- loompy/__init__.py
+++ loompy/__init__.py
@@ -1,3 +1,3 @@
-from .loompy import connect, create, combine, create_from_cellranger, LoomConnection
-from .loom_view import LoomView
 from .loom_layer import MemoryLoomLayer, LoomLayer
+from .loom_view import LoomView
+from .loompy import connect, create, combine, create_from_cellranger, LoomConnection
